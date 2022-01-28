f22cfcd0fa506fe66ea279576e4b3699eb01a86a
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2022-01-28 14:48:30 -0600

    changed varibale names

--- 01mapper.py
+++ 01mapper.py
@@ -2,15 +2,15 @@
 # Learning Mapper
 
 
-f = open("purchases.txt", "r")  # open file, read-only
-o = open("out01.txt", "w")  # open file, write
-for line in f:
+input = open("purchases.txt", "r")  # open file, read-only
+output = open("out01.txt", "w")  # open file, write
+for line in input:
     rowList = line.strip().split("    ")
     print(rowList)
     print(len(rowList))
     if len(rowList) == 6:
         date, time, location, dept, amount, payType = rowList  # assign names
         print("{0}\t{1}".format(location, amount))
-        o.write("{0}\t{1}\n".format(location, amount))
-f.close()
-o.close()
+        output.write("{0}\t{1}\n".format(location, amount))
+input.close()
+output.close()
